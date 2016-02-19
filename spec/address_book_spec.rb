4ada1a1deb294ae6803c8704cb3947c1fa84b420
Gahee Heo <ghbooth12@gmail.com>
2016-02-19 17:45:02 -0600

    assignment 19 models: completed

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -32,4 +32,17 @@ RSpec.describe AddressBook do
       expect(book.entries[0].email).to eq('augusta.king@lovelace.com')
     end
   end
+
+  describe "#remove_entry" do
+    let(:book) {AddressBook.new}
+
+    it "removes only one entry" do
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      old_size = book.entries.size
+      book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      new_size = book.entries.size
+      expect(old_size - new_size).to eq(1)
+    end
+  end
+
 end
